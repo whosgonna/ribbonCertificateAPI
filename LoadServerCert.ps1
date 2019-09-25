f00d90427fd2c9ef37dae1b2f6c396f7df145354
Ben Kaufman <ben.whosgonna.com@gmail.com>
2019-09-25 09:52:08 -0500

    Failing example for server cert added

---
+++ LoadServerCert.ps1
@@ -0,0 +1,108 @@
+param(
+    [Parameter(Mandatory=$true)] $ip,
+    [Parameter(Mandatory=$true)] $user,
+    [Parameter(Mandatory=$true)] $pass,
+    [Parameter(Mandatory=$true)] $P12File,   ## Filename of the certificate
+    [Parameter(Mandatory=$true)] $P12Pass    ## The passphrase for the certificate.
+)
+
+
+ ## Skip certificate validation.  This part is lengthly, but necessary since there is no
+ ## publicly trusted certificate installed on the SBC.
+ [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+ if (-not ([System.Management.Automation.PSTypeName]'ServerCertificateValidationCall./loback').Type)
+ {
+ $certCallback = @"
+     using System;
+     using System.Net;
+     using System.Net.Security;
+     using System.Security.Cryptography.X509Certificates;
+     public class ServerCertificateValidationCallback
+     {
+         public static void Ignore()
+         {
+             if(ServicePointManager.ServerCertificateValidationCallback ==null)
+             {
+                 ServicePointManager.ServerCertificateValidationCallback += 
+                     delegate
+                     (
+                         Object obj, 
+                         X509Certificate certificate, 
+                         X509Chain chain, 
+                         SslPolicyErrors errors
+                     )
+                     {
+                         return true;
+                     };
+             }
+         }
+     }
+"@
+     Add-Type $certCallback
+ }
+ [ServerCertificateValidationCallback]::Ignore()
+
+$props = @{};
+
+## Use -SkipCertificateCheck in powershell 6 and above
+if ( $PSVersionTable.PSVersion.Major -ge 6 ) {
+    $props = @{ SkipCertificateCheck = $true };
+}
+
+
+$urlRoot = "https://$ip/rest/";
+
+
+
+## Set the URL for logging in, and the http POST body
+$loginUrl = $urlRoot + "login";
+$body = @{
+    Username = $user;
+    Password = $pass;
+}
+
+## Log into the server.  The session data (including cookies), is saved in the $websession.
+Invoke-WebRequest -uri $loginUrl @props -SessionVariable websession -body $body -Method Post | Out-Null
+$props.websession = $websession
+
+
+## Using documentation from: https://support.sonus.net/display/UXAPIDOC/POST+certificate+-+action+import12
+## The following fields are required:
+##     CertFileName	- Indicates the name of the file to be uploaded with allowable file type extension such as .pfx or .p12.
+##     EncryptedPassword - Identifies the password for the password protected PKCS12 certificate.
+$body = @{
+    CertFileName      = $P12File;
+    EncryptedPassword = $P12Pass;
+}
+$certid = 1; ## Certificate id 1 is reserved for the server certificate (which is our goal here.)
+$certUrl = $urlRoot + "certificate/$($certid)?action=import12";
+Write-Host "`n`n$certUrl`n`n";
+$resp = Invoke-WebRequest @props -Uri $certUrl -Method Post -Body $body
+$resp.Content;
+
+## Log out to keep the number of active sessions on the SBC to a minimum.
+$logoutUrl = $urlRoot + 'logout'
+$resp = Invoke-WebRequest @props -Uri $logoutUrl  -Method Post | Out-Null
+
+$resp.Content;
+
+<#
+.\LoadServerCert.ps1 -ip $ip -user $user -pass $pass -P12File $certfile -P12Pass $certPass
+
+$resp.Content is:
+
+    <?xml version="1.0"?>
+    <root>
+    <status>
+    <http_code>500</http_code>
+    <app_status href="https://10.167.8.7/rest/certificate/1">
+    <app_status_entry code="15026" params="15000"/>
+    </app_status>
+    </status>
+    </root>
+
+Code 15026 is 'Certificate file data content cannot be empty or exceed the maximum size limit %1%.'
+
+
+
+#>
---
+++ README.md
@@ -0,0 +1,45 @@
+## Issue with uploading pfx files to Ribbon SWeLite using the API.
+
+
+All examples assume that the gateway's IP address is `10.167.8.7`, and that the
+API user name is `restApiUser` with a password of `P@ssW0rd`;
+
+### Example 1:  Uploading a trusted root certificate (.pem format) via the API 
+
+```LoadTrustedCert.ps1```
+
+**This example works and is provided for contrast to the examples that fail.**
+
+This is a fairly straight forward **working** example.  The certifiate data for the 
+Baltimore CyberTrust Root certificate is base64 data, so it's written directly
+into the script.  This is the certificate used by Microsoft on their Teams
+endpoints, so this certificate must be loaded as a trusted root certificate for
+any connection to Teams.
+
+In this example, the certificate data is base64, so it's put directly into the body
+of the request.  The script can be run as follows:
+
+```powershell
+.\LoadTrustedCert -ip '10.167.8.7' -user 'restApiUser' -pass 'P@ssW0rd'
+```
+
+
+### Example 2:  Uploading a pfx file as the server certificate.
+
+```LoadServerCert.psq```
+
+**This example fails.**
+
+This example is based very literally off of the documentation for importing a pfx
+file (https://support.sonus.net/display/UXAPIDOC/POST+certificate+-+action+import12), 
+which lists only two parameters, `CertFileName` and `EncryptedPassword`.  If the 
+certificate file is `C:\certificate.pfx` with a password of `C3rtP@ss`, the script
+would be executed as follows:
+
+```powershell
+.\LoadServerCert.ps1 -ip '10.167.8.7' -user 'restApiUser' -pass 'P@ssW0rd' -P12File 'C:\certificate.pfx' -P12Pass 'C3rtP@ss'
+```
+
+This fails with a code of `15026 - Certificate file data content cannot be empty or 
+exceed the maximum size limit %1%.`.
+
